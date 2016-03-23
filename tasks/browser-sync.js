7c8388e7c8c33f4faff74711201a5baf64ff779a
Daniel Reid <danielc.reid@gmail.com>
2016-03-23 12:38:02 -0500

    Update browser-sync.js

--- tasks/browser-sync.js
+++ tasks/browser-sync.js
@@ -4,12 +4,14 @@ var config = require('./config');
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var historyApiFallback = require('connect-history-api-fallback');
 
 module.exports = function() {
   return function() {
     browserSync.init({
       server: {
-        baseDir: './' + config.destination
+        baseDir: './' + config.destination,
+        middleware: [historyApiFallback()]
       },
       reloadDelay: 200
     });
